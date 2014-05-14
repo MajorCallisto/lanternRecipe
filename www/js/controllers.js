07e93d8d4be324a06fc9864cea04336e215290e8
Brendan Neufeld <brendanjneufeld@gmail.com>
2014-05-14 00:53:42 -0400

    added views for basic app layout

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,23 +1,50 @@
 angular.module('starter.controllers', [])
 
-.controller('AboutCtrl', function($scope) {
-       // rfduino.discover(5, app.onDiscoverDevice, app.onError);
+    .controller('IntroductionCtrl', function ($scope, $state, StaticText, UserInfo) {
+        $scope.staticText = StaticText.get($state.$current.toString());
+        $scope.userInfo = UserInfo.all();
+        console.log($scope.userInfo);
+        $scope.submit = function () {
+            $state.go('tab.about');
+            UserInfo.set()
+            //console.log()
+            //alert('submit')
+            //window.location.href = '#/tab/about/';
+            //$location.url('#/tab/about/')
+        }
+    })
+
+    .controller('AboutCtrl', function ($scope, $state, StaticText) {
+        $scope.staticText = StaticText.get('about');
+        console.log($scope.staticText.ol1);
+        // rfduino.discover(5, app.onDiscoverDevice, app.onError);
+        console.log($state);
+
+    })
+
+    .controller('IngredientsCtrl', function ($scope, $state, Ingredients, StaticText) {
+        $scope.staticText = StaticText.get('ingredients');
+        $scope.ingredients = Ingredients.all();
+        console.log($state);
+    })
+
+    .controller('IngredientDetailCtrl', function ($scope, $stateParams, $state, Ingredients, StaticText) {
+        $scope.staticText = StaticText.get($state.$current.toString());
+        $scope.ingredient = Ingredients.get($stateParams.ingredientId);
+        console.log($state);
+    })
+
+    .controller('ProfileCtrl', function ($scope, $state, StaticText, UserInfo) {
+        $scope.staticText = StaticText.get('profile');
+        $scope.userInfo = UserInfo.all();
+        $scope.submit = function () {
+            $state.go('tab.about');
+            //console.log()
+            //alert('submit')
+            //window.location.href = '#/tab/about/';
+            //$location.url('#/tab/about/')
+        }
+        console.log($state);
+    });
 
 
-})
-
-.controller('IngredientsCtrl', function($scope, Ingredients) {
-  $scope.ingredients = Ingredients.all();
-})
-
-.controller('IngredientDetailCtrl', function($scope, $stateParams, Ingredients) {
-        //console.log($stateParams.ingredientId)
-       // console.log(Ingredients)
-       //console.log('$stateParams: ',$stateParams);
-       // console.log('Ingredients: ',Ingredients);
-       // console.log('ingredient: ',Ingredients.get($stateParams.ingredientId))
-  $scope.ingredient = Ingredients.get($stateParams.ingredientId);
-})
-
-.controller('ProfileCtrl', function($scope) {
-});
